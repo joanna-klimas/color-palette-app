878e1554abad4fe8fe5a997926784e9f5599fff1
Joanna Klimas <klimas.j.m@gmail.com>
2019-06-27 19:54:38 +1200

    fixed

--- client/api.js
+++ client/api.js
@@ -1,17 +1,18 @@
-// import request from 'superagent'
-// import color from '../componenets/Home.jsx' // this needs to import color chosen by user as a base color from one of the componenets
 const request = require('superagent')
+// import color from '../componenets/Home.jsx' // this needs to import color chosen by user as a base color from one of the componenets
 
-const monochromeUrl = ' http://thecolorapi.com/scheme?hex=0047AB&format=json&mode=monochrome&count=3'
+const monochromeUrl = 'http://thecolorapi.com/scheme?hex=0047AB&format=json&mode=monochrome&count=3'
 //  in the future this will be replaced by http://thecolorapi.com/scheme?hex=${users_color}&format=json&mode=monochrome&count=3
 
-function newMonochromePalette (callback) {
-  return request
+function newMonochromePalette () {
+  request
     .get(monochromeUrl)
     .end((err, res) => {
       console.log(res.body)
-      callback(err, res.body)
+      return res.body
     })
 }
 
-module.exports = newMonochromePalette
+module.exports = {
+  newMonochromePalette
+}
--- server/server.js
+++ server/server.js
@@ -1,18 +1,15 @@
-// import { newMonochromePalette } from '../client/api.js'
-const api = require('../client/api.js')
 const path = require('path')
 const express = require('express')
-const cors = require('cors')
 
 const server = express()
-//const router = express.Router()
+
+const api = require('../client/api')
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
-server.use(cors('*'))
 
 server.get('/api', (req, res) => {
-  api.newMonochromePalette()
+  api.newMonochromePalette ()
     .then(data => {
       console.log(data)
       res.send(data)
