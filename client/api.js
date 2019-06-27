cf84837d32e6af488d06e73d025c3748f2165dc6
Joanna Klimas <klimas.j.m@gmail.com>
2019-06-27 20:14:01 +1200

    Revert "fixed"
    This reverts commit 878e1554abad4fe8fe5a997926784e9f5599fff1.

--- client/api.js
+++ client/api.js
@@ -1,18 +1,17 @@
-const request = require('superagent')
+// import request from 'superagent'
 // import color from '../componenets/Home.jsx' // this needs to import color chosen by user as a base color from one of the componenets
+const request = require('superagent')
 
-const monochromeUrl = 'http://thecolorapi.com/scheme?hex=0047AB&format=json&mode=monochrome&count=3'
+const monochromeUrl = ' http://thecolorapi.com/scheme?hex=0047AB&format=json&mode=monochrome&count=3'
 //  in the future this will be replaced by http://thecolorapi.com/scheme?hex=${users_color}&format=json&mode=monochrome&count=3
 
-function newMonochromePalette () {
-  request
+function newMonochromePalette (callback) {
+  return request
     .get(monochromeUrl)
     .end((err, res) => {
       console.log(res.body)
-      return res.body
+      callback(err, res.body)
     })
 }
 
-module.exports = {
-  newMonochromePalette
-}
+module.exports = newMonochromePalette
--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
+// import { newMonochromePalette } from '../client/api.js'
+const api = require('../client/api.js')
 const path = require('path')
 const express = require('express')
+const cors = require('cors')
 
 const server = express()
-
-const api = require('../client/api')
+//const router = express.Router()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+server.use(cors('*'))
 
 server.get('/api', (req, res) => {
-  api.newMonochromePalette ()
+  api.newMonochromePalette()
     .then(data => {
       console.log(data)
       res.send(data)
